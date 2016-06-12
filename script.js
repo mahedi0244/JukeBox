63369af08baa42f5f4f0ecdf740ca2e5588bd0eb
Mahedi <mahedi0244@gmail.com>
2016-06-12 19:36:32 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,78 @@
+
+var playlist = new Array("music1.mp3", "music2.mp3", "music3.mp3", "music4.mp3","music5.mp3");
+var count = 0;
+
+$(document).ready(function(){
+	
+	$(".activate-modal").click(function(e){
+	    e.preventDefault(); //dont follow the link
+	    $(".modal-wrapper").show(); //show our modal
+	    //add the .modal-on class to <body>
+	    $("body").addClass("modal-on");   
+  	});
+	 $(".exit").click(function(e){
+	    e.preventDefault();
+		//basically the opposite of activate
+	    $(".modal-wrapper").hide();
+	    $("body").removeClass("modal-on");
+	 });
+	
+  	var myJuke = new Jukebox();
+  	$( "#play" ).click(function() {
+ 		myJuke.play();
+	});
+	$( "#pause" ).click(function() {
+  		myJuke.pause();
+	});
+	$( "#stop" ).click(function() {
+  		myJuke.stop();
+	});
+	$( "#loadButton" ).click(function() {
+  		myJuke.load();
+	});
+
+	$("#loadButton").click(function(e){
+	    e.preventDefault();
+	    myJuke.load();
+		//basically the opposite of activate
+	    $(".modal-wrapper").hide();
+	    $("body").removeClass("modal-on");
+	 });
+	
+});
+
+function Jukebox(){   
+	var x = nextAudio();
+	this.play = function() { 
+    	x.play(); 
+	} 
+	this.pause = function(){
+		x.pause();
+	}
+	this.stop = function(){
+		x.pause();
+		x.currentTime = 0;
+	}
+	this.load = function(){
+		var path = document.getElementById("usrinput").value;
+		audio = document.getElementById("myAudio");
+		audio.src = path;
+	}
+}
+
+function nextAudio() { 
+   	var audio = document.getElementById("myAudio");
+	audio.src = playlist[count];
+	count = (count+1)%(playlist.length);
+
+	return audio;
+} 
+
+
+
+
+
+
+
+
+
